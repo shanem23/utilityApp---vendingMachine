580b740f4c92fa3689ac1ee906763ba1794945c4
shanem23 <147488676+shanem23@users.noreply.github.com>
2024-01-30 21:02:54 +0000

    Commit

--- Vending Machine/Vending Machine/main.cpp
+++ Vending Machine/Vending Machine/main.cpp
@@ -10,17 +10,17 @@ bool checkStock(const Product& product) {
     return (product.stock > 0);
 }
 
-
 void displayMenu(const Product& product) {
     cout << "|| " << product.name << " | Stock: " << product.stock << " || " << endl;
 }
 
-
-void purchaseItem(Product& item, int& currentBalance) { // Function which is used when user selects an item to purchase
+// Function which is used when user selects an item to purchase
+void purchaseItem(Product& item, int& currentBalance) {
     int itemCost;
 
+    // If item is in stock and the user has enough balance, the users balance and the items stock is updated then the item is 'dispensed'
     if (checkStock(item) && currentBalance >= item.cost) {
-        // If item is in stock and the user has enough balance, the users balance and the items stock is updated then the item is 'dispensed'
+
         itemCost = item.cost;
         item.stock--;
         currentBalance -= itemCost;
@@ -33,21 +33,13 @@ void purchaseItem(Product& item, int& currentBalance) { // Function which is use
     }
 }
 
-int enterMoney(int currentBalance) { // Simple function for topping up balance by taking the balances current value and adding on the entered amount of new balance
-    int newMoney;
-
-    cout << "How much money would you like to enter? Enter the amount in pence: ";
-    cin >> newMoney;
-
-    currentBalance = newMoney + currentBalance;
-    return currentBalance;
-}
-
-void foodMenu(Product& food1, Product& food2, Product& food3, Product& food4, Product& food5, int& currentBalance) { // Main menu for the purchasing of food.
+// Main menu for the purchasing of food.
+void foodMenu(Product& food1, Product& food2, Product& food3, Product& food4, Product& food5, int& currentBalance) {
     int menuChoice;
     char continueShopping;
 
-    do { // Food menu is printed with the displayMenu function being used to print out the item name and stock for each item after its corresponding number
+    // Food menu is printed with the displayMenu function being used to print out the item name and stock for each item after its corresponding number
+    do {
         cout << "Food Menu:" << endl;
         cout << "1. ";
         displayMenu(food1);
@@ -63,7 +55,8 @@ void foodMenu(Product& food1, Product& food2, Product& food3, Product& food4, Pr
 
         cin >> menuChoice;
 
-        switch (menuChoice) { // Depending on what item the user selects, the purchaseItem function is run with the item of choice and the users current balance
+        // Depending on what item the user selects, the purchaseItem function is run with the item of choice and the users current balance
+        switch (menuChoice) {
         case 1:
             purchaseItem(food1, currentBalance);
             break;
@@ -86,7 +79,8 @@ void foodMenu(Product& food1, Product& food2, Product& food3, Product& food4, Pr
             cout << "Invalid choice. Please enter a valid option." << endl;
         }
 
-        if (currentBalance >= 0) { // through the use of a while loop,if  the users balance is 0 or above, the user will be asked if they wish to continue shopping
+        // through the use of a while loop,if  the users balance is 0 or above, the user will be asked if they wish to continue shopping
+        if (currentBalance >= 0) {
             cout << "Do you want to continue shopping? (y/n): ";
             cin >> continueShopping;
         } else {
@@ -101,7 +95,8 @@ void drinkMenu(Product& drink1, Product& drink2, Product& drink3, Product& drink
     int menuChoice;
     char continueShopping;
 
-    do { //Just like the food menu, the drinks menu is printed with the displayMenu function being used to print out the item name and stock for each item after its corresponding number
+    //Just like the food menu, the drinks menu is printed with the displayMenu function being used to print out the item name and stock for each item after its corresponding number
+    do {
         cout << "Drink Menu:" << endl;
         cout << "1. ";
         displayMenu(drink1);
@@ -117,7 +112,7 @@ void drinkMenu(Product& drink1, Product& drink2, Product& drink3, Product& drink
 
         cin >> menuChoice;
 
-        switch (menuChoice) {
+        switch (menuChoice) { //Switch statement depending on which item the user selects to purchase
         case 1:
             purchaseItem(drink1, currentBalance);
             break;
@@ -151,6 +146,18 @@ void drinkMenu(Product& drink1, Product& drink2, Product& drink3, Product& drink
     } while (continueShopping == 'y' || continueShopping == 'Y');
 }
 
+// Simple function for topping up balance by taking the balances current value and adding on the entered amount of new balance
+int enterMoney(int currentBalance) {
+    int newMoney;
+
+    cout << "How much money would you like to enter? Enter the amount in pence: ";
+    cin >> newMoney;
+
+    currentBalance = newMoney + currentBalance;
+    return currentBalance;
+}
+
+//Function for printing main menu of program, uses file handling to retrieve the vending machine graphic from mainMenu.txt
 void mainMenu(Product &food1, Product &food2, Product &food3, Product &food4, Product &food5, Product &drink1, Product &drink2, Product &drink3, Product &drink4, Product &drink5, int &currentBalance){
     
     int menuChoice;
@@ -172,7 +179,7 @@ void mainMenu(Product &food1, Product &food2, Product &food3, Product &food4, Pr
     cout << "3. Top-up Money" << endl;
     cin >> menuChoice;
     
-    switch (menuChoice) {
+    switch (menuChoice) { //Switch statement for different user inputs
         case 1:
             foodMenu(food1, food2, food3, food4, food5, currentBalance);
             break;
@@ -187,7 +194,9 @@ void mainMenu(Product &food1, Product &food2, Product &food3, Product &food4, Pr
             cout << "Invalid choice. Exiting." << endl;
     }
 }
- int main() { // Objects are created for each of the food and drink items in the vending machine, an initial user balance is created, the menu function is ran with all of the new values being passed in
+
+// Objects are created for each of the food and drink items in the vending machine, an initial user balance is set, the menu function is ran with all of the new objects being passed in
+ int main() {
      Product drink1("Coke", 200, 5);
      Product drink2("Water", 150, 5);
      Product drink3("Fanta", 150, 5);
@@ -201,6 +210,6 @@ void mainMenu(Product &food1, Product &food2, Product &food3, Product &food4, Pr
 
      int currentBalance = 500;
  
-     mainMenu(drink1, drink2, drink3, drink4, drink5, food1, food2, food3, food4, food5, currentBalance);
+     mainMenu(food1, food2, food3, food4, food5, drink1, drink2, drink3, drink4, drink5, currentBalance);
      
   }
